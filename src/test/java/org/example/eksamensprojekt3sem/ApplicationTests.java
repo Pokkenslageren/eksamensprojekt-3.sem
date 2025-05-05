cda361fcf169cf4225cd52ddd7ace06a960d578f
Kalle-KO <kaot0001@stud.kea.dk>
2025-05-05 10:32:07 +0200

    Initial commit

---
+++ src/test/java/org/example/eksamensprojekt3sem/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamensprojekt3sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
