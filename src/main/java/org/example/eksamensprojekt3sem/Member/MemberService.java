34c88639ed5c41f49bc87fd057202c4b6ec54e1f
Kalle-KO <kaot0001@stud.kea.dk>
2025-05-08 09:54:00 +0200

    Initial setup of Member

---
+++ src/main/java/org/example/eksamensprojekt3sem/Member/MemberService.java
@@ -0,0 +1,13 @@
+package org.example.eksamensprojekt3sem.Member;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+}
