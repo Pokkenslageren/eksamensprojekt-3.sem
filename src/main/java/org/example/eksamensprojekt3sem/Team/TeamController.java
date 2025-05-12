2235264b72fc7693370a03c30271160cc85f4a16
Kalle-KO <kaot0001@stud.kea.dk>
2025-05-12 13:57:14 +0200

    CRUD Team

--- src/main/java/org/example/eksamensprojekt3sem/Team/TeamController.java
+++ src/main/java/org/example/eksamensprojekt3sem/Team/TeamController.java
@@ -1,23 +1,51 @@
 package org.example.eksamensprojekt3sem.Team;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.example.eksamensprojekt3sem.Member.MemberModel;
+import org.example.eksamensprojekt3sem.Member.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("")
 public class TeamController {
 
     private final TeamService teamService;
+    private final MemberService memberService;
+    private final TeamRepository teamRepository;
 
-    public TeamController(TeamService teamService) {
+    public TeamController(TeamService teamService, MemberService memberService, TeamRepository teamRepository) {
         this.teamService = teamService;
+        this.memberService = memberService;
+        this.teamRepository = teamRepository;
     }
 
     @GetMapping("/teams")
     public List<TeamModel> getAllTeams() {
         return teamService.getAllTeams();
+    }
+
+    @PostMapping("/teams/add")
+    public MemberModel addMemberToTeam(@Valid @RequestBody MemberModel member) {
+        return teamService.addMember(member);
+    }
+
+    @PutMapping("/teams/update")
+    public ResponseEntity<TeamModel> updateTeam(@PathVariable long id, @Valid @RequestBody TeamModel teamDetails) {
+        return teamService.updateTeam(id, teamDetails)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/teams/delete/{id}")
+    public ResponseEntity<TeamModel> deleteTeam(@PathVariable long id) {
+        if (teamService.deleteTeam(id)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
